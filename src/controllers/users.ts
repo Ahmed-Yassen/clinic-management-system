96e64c6ae5e96e1ded1582149a3ac2350b5806d5
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-08-12 19:44:06 +0200

    Add createDoctor endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,8 @@ import { Response, NextFunction } from "express";
 import { Receptionists } from "../models/receptionists";
 import { throwCustomError } from "../utils/helperFunctions";
 import { Users } from "../models/users";
+import { Doctors } from "../models/doctors";
+import { Specialties } from "../models/specialties";
 
 export default class UserController {
   constructor() {}
@@ -52,6 +54,36 @@ export default class UserController {
     } catch (error) {
       //- if created row in user table, but an error happened during creating receptionist , remove the row that was created in the users table
       if (user && !receptionist)
+        await Users.destroy({ where: { email: user.email } });
+      next(error);
+    }
+  };
+
+  createDoctor = async (req: any, res: Response, next: NextFunction) => {
+    let user: Users | null = null;
+    let doctor: Doctors | null = null;
+    try {
+      if (await this.userEmailExists(req.body.email))
+        throwCustomError("Email already exists!", 400);
+
+      const specialty = await Specialties.findByPk(req.body.SpecialtyId);
+      if (!specialty)
+        throwCustomError("There is no specialty with that id", 404);
+
+      const [userTableData, roleTableData] = this.seperateData(
+        req.body,
+        "doctor"
+      );
+      user = await this.createUser(userTableData);
+      roleTableData.UserId = user?.id; //- Attach Foreign Key from users
+      doctor = await Doctors.create(roleTableData);
+      res.status(201).json({
+        success: true,
+        fullData: { user, doctor },
+      });
+    } catch (error) {
+      //- if created row in user table, but an error happened in during creating a doctor, remove the row that was created in the users table
+      if (user && !doctor)
         await Users.destroy({ where: { email: user.email } });
       next(error);
     }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -34,4 +34,33 @@ router.post(
   controller.createReceptionist
 );
 
+router.post(
+  "/signup/doctor",
+  authMW,
+  isAdmin,
+  [
+    body("email")
+      .isEmail()
+      .toLowerCase()
+      .withMessage("Please provide a valid email"),
+    body("password")
+      .isString()
+      .isLength({ min: 8, max: 32 })
+      .withMessage("Password should be between 8 and 32 string characters"),
+    body("phoneNumber")
+      .matches(/^01[0125][0-9]{8}$/)
+      .withMessage("Incorrect phoneNumber value."),
+    body("address").isString().withMessage("Incorrect address value."),
+    body("fullName").isString().withMessage("Incorrect fullName value."),
+    body("examinationPrice")
+      .isFloat({ min: 50 })
+      .withMessage("Incorrect ExaminationPrice value."),
+    body("SpecialtyId")
+      .isInt({ min: 1 })
+      .withMessage("Incorrect SpecialtyId value."),
+  ],
+  validationMW,
+  controller.createDoctor
+);
+
 export default router;

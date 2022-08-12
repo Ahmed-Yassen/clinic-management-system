c0c26c7b89a34bf284501af7827be022423f52c5
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-08-12 19:23:01 +0200

    Add express app boilerplate

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/../config/dev.env" });
+
+let app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: "URL NOT FOUND!" });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  let status = err.status || 500;
+  res.status(status).json({ success: false, message: err.message });
+});
+
+export default app;

cee44fea57a2ee54bcd5f2989842555863c1b1ba
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-08-12 21:48:39 +0200

    Finish receptionists unit tests

---
+++ src/tests/receptionists.test.ts
@@ -0,0 +1,27 @@
+import { agent as request } from "supertest";
+import app from "../app";
+import { populateTestingDB, receptionistToken } from "./fixtures/db";
+import connection from "../db";
+
+beforeEach(async () => {
+  await connection.dropAllSchemas({ logging: false });
+  await connection.sync();
+  await populateTestingDB();
+});
+
+test("Should update receptionist profile as receptionist", async () => {
+  const response = await request(app)
+    .patch("/receptionists")
+    .set("Authorization", `Bearer ${receptionistToken}`)
+    .send({
+      phoneNumber: "01234567890",
+      fullName: "Receptionist Pam",
+      address: "USA",
+    })
+    .expect(200);
+  expect(response.body.receptionist).toMatchObject({
+    phoneNumber: "01234567890",
+    fullName: "Receptionist Pam",
+    address: "USA",
+  });
+});

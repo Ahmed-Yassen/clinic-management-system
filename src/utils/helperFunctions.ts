1cb34340c0867dd191b7fa0e006fdd457aca562e
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-08-13 14:23:40 +0200

    Add toGMT2 & isOffDay helperFunctions

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,7 +1,17 @@
+import moment from "moment";
+
 const throwCustomError = (errorMessage: string, errorStatus: number) => {
   const error: any = new Error(errorMessage);
   error.status = errorStatus;
   throw error;
 };
 
-export { throwCustomError };
+const isOffDay = (date: Date): Boolean => {
+  return date.toDateString().match(/^(fri|sat)/i) !== null;
+};
+
+const toGMT2 = (date: Date): Date => {
+  return moment(new Date(date)).add(2, "hours").toDate();
+};
+
+export { throwCustomError, toGMT2, isOffDay };

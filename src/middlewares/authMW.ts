2974bfec23355cfa6780d34f91c7b7d2d5958ed5
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-10-17 14:18:07 +0200

    refactor auth middleware

--- src/middlewares/authMW.ts
+++ src/middlewares/authMW.ts
@@ -1,24 +1,25 @@
 import { NextFunction, Response } from "express";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
-import { Users } from "../models/users";
+import { NotAuthenticatedError } from "../errors/not-authenticated.error";
+import { NotFoundError } from "../errors/not-found-error";
+import { User } from "../models/user";
 import { throwCustomError } from "../utils/helperFunctions";
 
+interface UserPayload {
+  id: string;
+}
 export default async (req: any, res: Response, next: NextFunction) => {
-  try {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
-    if (!token) throwCustomError("Unauthenticated!", 401);
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) throw new NotAuthenticatedError();
 
-    const decodedToken = jwt.verify(
-      token,
-      process.env.JWT_SECRET as Secret
-    ) as JwtPayload;
-    const user = await Users.findByPk(decodedToken.id);
-    if (!user) throwCustomError("Couldnt find a user with that id", 404);
+  const decodedToken = jwt.verify(
+    token,
+    process.env.JWT_SECRET as Secret
+  ) as UserPayload;
 
-    req.role = user?.role;
-    req.user = user;
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const user = await User.findByPk(decodedToken.id);
+  if (!user) throw new NotFoundError("user");
+
+  req.user = user;
+  next();
 };

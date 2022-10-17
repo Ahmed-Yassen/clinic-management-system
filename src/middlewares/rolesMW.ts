1fb821761bfce76012c2b8fc92cac3a13e030f09
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-10-17 14:18:51 +0200

    refactor roles middleware

--- src/middlewares/rolesMW.ts
+++ src/middlewares/rolesMW.ts
@@ -1,25 +1,20 @@
 import { Response, NextFunction } from "express";
+import { NotAuthorizedError } from "../errors/not-authorized-error";
 import { throwCustomError } from "../utils/helperFunctions";
 
 const isAdmin = (req: any, res: Response, next: NextFunction) => {
-  req.role === "admin"
-    ? next()
-    : throwCustomError("Forbidden Operation!, Only Admins can access", 403);
+  if (req.user.role === "admin") return next();
+  throw new NotAuthorizedError();
 };
 
 const isDoctor = (req: any, res: Response, next: NextFunction) => {
-  req.role === "doctor"
-    ? next()
-    : throwCustomError("Forbidden Operation!, Only Doctors can access", 403);
+  if (req.user.role === "doctor") return next();
+  throw new NotAuthorizedError();
 };
 
 const isReceptionist = (req: any, res: Response, next: NextFunction) => {
-  req.role === "receptionist"
-    ? next()
-    : throwCustomError(
-        "Forbidden Operation!, Only Receptionists can access",
-        403
-      );
+  if (req.user.role === "receptionist") return next();
+  throw new NotAuthorizedError();
 };
 
 export { isAdmin, isDoctor, isReceptionist };
